8cbf7ea26b84d689e1b707fcfab8f207875ec522
Joe Bellus <joe@5sigma.io>
2018-06-20 16:40:07 -0400

    Method based routing (#13)
    Routing should obey the http method in the request. Previously the
    method was ignored and route matching was done strictly on path. This
    has been changed so that a route no longer matches if the http method
    verb is not correct. This will cause URLs hit with a method different
    from what is specified in the routes to return a 404 error.
    
    This changed forced a change to the scope and router API. The handle
    functions now accept the http.Request instead of a path string.
    Middleware and the testing package were updated to compensate.
    
    Resolves #10

--- router.go
+++ router.go
@@ -1,18 +1,22 @@
 package celerity
 
+import "net/http"
+
 var (
-	// GET - GET verb for HTTP requests
+	// GET verb for HTTP requests
 	GET = "GET"
-	// POST - POST verb for HTTP request
+	// POST verb for HTTP request
 	POST = "POST"
-	// PUT - PUT verb for HTTP request
+	// PUT verb for HTTP request
 	PUT = "POST"
-	// DELETE - DELETE verb for HTTP request
+	// DELETE verb for HTTP request
 	DELETE = "POST"
+	// ANY can be used to match any method
+	ANY = "*"
 )
 
-//MiddlewareHandler - A middleware function that can be used in scopes and
-//routes
+//MiddlewareHandler is a function that can be used in scopes and
+//routes to transform the context before the route is processed.
 type MiddlewareHandler func(RouteHandler) RouteHandler
 
 // Router - The server router stores all routes, groups, and determines what to
@@ -29,8 +33,8 @@ func NewRouter() *Router {
 }
 
 // Handle - Process the inccomming URL and execute the first matching route
-func (r *Router) Handle(c Context, path string) Response {
-	return r.Root.Handle(c, path)
+func (r *Router) Handle(c Context, req *http.Request) Response {
+	return r.Root.Handle(c, req)
 }
 
 // Route - Create a route on the root scope

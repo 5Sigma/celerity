cc50af39aded6c5f4308838b258cd022960df93a
Joe Bellus <joe@5sigma.io>
2018-07-19 12:38:25 -0400

    Refactored request logger
    Request logger has been refactored to use the vox library and provide
    more configuration

--- middleware/request_logger_test.go
+++ middleware/request_logger_test.go
@@ -1,11 +1,11 @@
 package middleware
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/5Sigma/celerity"
 	"github.com/5Sigma/celerity/celeritytest"
+	"github.com/5Sigma/vox"
 )
 
 func TestConsoleOutput(t *testing.T) {
@@ -15,14 +15,16 @@ func TestConsoleOutput(t *testing.T) {
 	})
 
 	config := NewLoggerConfig()
-	out := new(bytes.Buffer)
-	svr.Log.SetOutput(out)
+	v := vox.New()
+	pipeline := v.Test()
+	config.Log = v
 
 	svr.Use(RequestLoggerWithConfig(config))
 
 	celeritytest.Get(svr, "/foo")
 
-	if out.Len() < 54 {
-		t.Errorf("output length was not correct: %d, should be > 54", out.Len())
+	if len(pipeline.All()) < 54 {
+		t.Errorf("output length was not correct: %d, should be > 54\n%s",
+			len(pipeline.Last()), pipeline.Last())
 	}
 }
